370a71aa7caa95a1d39c17020412e5a3392f03e8
montehurd <mhurd@wikimedia.org>
2019-05-01 16:11:48 -0700

    apply lint auto-fixes

--- routes/page/talk.js
+++ routes/page/talk.js
@@ -38,52 +38,52 @@ function fetchDocAndRevision(app, req) {
     .then(doc => [doc, revision]);
 }
 
-const depthIndicatingAncestorTags = ['DL', 'UL', 'OL']
+const depthIndicatingAncestorTags = ['DL', 'UL', 'OL'];
 function getDepth(element) {
-  let elem = element
-  let familyTreeTags = [element.tagName] 
-  while(elem = elem.parentElement) { 
-    familyTreeTags.push(elem.tagName) 
+  let elem = element;
+  let familyTreeTags = [element.tagName];
+  while (elem = elem.parentElement) {
+    familyTreeTags.push(elem.tagName);
   }
-  let depth = familyTreeTags.filter(tag => depthIndicatingAncestorTags.includes(tag)).length
+  let depth = familyTreeTags.filter(tag => depthIndicatingAncestorTags.includes(tag)).length;
   if (element.tagName === 'DT') {
-    depth = depth - 1
+    depth = depth - 1;
   }
-  return depth
+  return depth;
 }
 
-const consecutiveWhitespaceLinesRegex = /\n\s*\n/g 
-const signatureRegex = /.*\s+\d{4}\s+\(.*\)\s*$/
+const consecutiveWhitespaceLinesRegex = /\n\s*\n/g;
+const signatureRegex = /.*\s+\d{4}\s+\(.*\)\s*$/;
 
 class WMFMessage {
   constructor(text = '', depth = 0) {
-    this.text = text.replace(consecutiveWhitespaceLinesRegex, '\n').trim() 
-    this.depth = depth
-    this.sha = createSha1(this.text)
+    this.text = text.replace(consecutiveWhitespaceLinesRegex, '\n').trim();
+    this.depth = depth;
+    this.sha = createSha1(this.text);
   }
 }
 
 class WMFMessageFragmentAndDepth {
   constructor(fragment = null, depth = 0) {
-    this.fragment = fragment
-    this.depth = depth
-    this.fragmentEndsWithSig = this.endsWithSig()
+    this.fragment = fragment;
+    this.depth = depth;
+    this.fragmentEndsWithSig = this.endsWithSig();
   }
   endsWithSig() {
     if (this.fragment === null) {
-      return false
+      return false;
     }
-    return signatureRegex.test(this.fragment.textContent)
+    return signatureRegex.test(this.fragment.textContent);
   }
   appendChildren(children) {
-    children.forEach(child => this.fragment.appendChild(child))
+    children.forEach(child => this.fragment.appendChild(child));
   }
   prependChildren(children) {
-    children.forEach(child => this.fragment.insertBefore(child, this.fragment.firstChild))
+    children.forEach(child => this.fragment.insertBefore(child, this.fragment.firstChild));
   }
 }
 
-// if a fragment's content text ends in 4 digits followed by parenthetical content 
+// if a fragment's content text ends in 4 digits followed by parenthetical content
 // ( ie: '2018 (CEST)' ) then we're considering it a separate item. (`fragmentEndsWithSig` can be
 // checked to make this determination.) items that do not end with such a signature need to be
 // combined with either the next or previous item with such a signature. This `combiner` uses an
@@ -91,179 +91,179 @@ class WMFMessageFragmentAndDepth {
 // an item with a signature is encountered - as such can be used with array 'filter' so it both
 // does the desired combination but also removes fragments from the array when their contents are
 // appended to other fragments.
-const accumulator = []
-let firstItemEndsWithSig = false
+const accumulator = [];
+let firstItemEndsWithSig = false;
 const combiner = (fragmentAndDepth, index, array, doc) => {
   if (index === 0) {
-    accumulator.length = 0
-    firstItemEndsWithSig = fragmentAndDepth.fragmentEndsWithSig 
+    accumulator.length = 0;
+    firstItemEndsWithSig = fragmentAndDepth.fragmentEndsWithSig;
   }
-  
-  let stopAccumulating = false
+
+  let stopAccumulating = false;
   if (index + 1 === array.length) {
-    stopAccumulating = true
+    stopAccumulating = true;
   } else {
-    const nextFragmentAndDepth = array[index + 1]
-    
-    if (fragmentAndDepth.fragmentEndsWithSig){
-      stopAccumulating = nextFragmentAndDepth.fragmentEndsWithSig === fragmentAndDepth.fragmentEndsWithSig
+    const nextFragmentAndDepth = array[index + 1];
+
+    if (fragmentAndDepth.fragmentEndsWithSig) {
+      stopAccumulating = nextFragmentAndDepth.fragmentEndsWithSig === fragmentAndDepth.fragmentEndsWithSig;
     } else {
-      stopAccumulating = nextFragmentAndDepth.fragmentEndsWithSig != fragmentAndDepth.fragmentEndsWithSig
+      stopAccumulating = nextFragmentAndDepth.fragmentEndsWithSig != fragmentAndDepth.fragmentEndsWithSig;
     }
-  } 
-  
+  }
+
   if (stopAccumulating) {
-    accumulator.forEach(accumulatedFragmentAndDepth => { 
-      const tabsTextNode = doc.createTextNode(`\n${'\t'.repeat(accumulatedFragmentAndDepth.depth)}`)
-      fragmentAndDepth.appendChildren([tabsTextNode, accumulatedFragmentAndDepth.fragment])
-    })
-    accumulator.length = 0
+    accumulator.forEach(accumulatedFragmentAndDepth => {
+      const tabsTextNode = doc.createTextNode(`\n${'\t'.repeat(accumulatedFragmentAndDepth.depth)}`);
+      fragmentAndDepth.appendChildren([tabsTextNode, accumulatedFragmentAndDepth.fragment]);
+    });
+    accumulator.length = 0;
   } else {
-    accumulator.unshift(fragmentAndDepth)
-  } 
-  return stopAccumulating
-} 
+    accumulator.unshift(fragmentAndDepth);
+  }
+  return stopAccumulating;
+};
 
 function removeAttributes(node, attributes) {
-  attributes.forEach(attribute => node.removeAttribute(attribute))
+  attributes.forEach(attribute => node.removeAttribute(attribute));
 }
 
-const textContentTagsToPreserve = ['A', 'B', 'I']
-const textContentTagsToConvertToBold = ['DT']
-const attributesToRemove = ['style','id','class','rel','about']
+const textContentTagsToPreserve = ['A', 'B', 'I'];
+const textContentTagsToConvertToBold = ['DT'];
+const attributesToRemove = ['style','id','class','rel','about'];
 function textContent(rootNode, doc, exclusions = []) {
   if (!rootNode) {
-    return ''
+    return '';
   }
   const childNodes = rootNode.childNodes;
   if (!childNodes) {
-    return ''
+    return '';
   }
   const results = [];
-  const tagsToPreserve = textContentTagsToPreserve.filter(tag => !exclusions.includes(tag))
+  const tagsToPreserve = textContentTagsToPreserve.filter(tag => !exclusions.includes(tag));
   childNodes.forEach(childNode => {
     if (childNode.nodeType === 3) {
-      const text = childNode.nodeValue
+      const text = childNode.nodeValue;
       const escapedText = text
         .replace(/&/g,'&amp;')
         .replace(/</g,'&lt;')
-        .replace(/>/g,'&gt;')
-      results.push(escapedText)
+        .replace(/>/g,'&gt;');
+      results.push(escapedText);
     } else if (childNode.nodeType === 1) {
       // Everything should be text except `tagsToPreserve`
-      if(tagsToPreserve.includes(childNode.tagName)) {
-        const clone = childNode.cloneNode(true)
-        removeAttributes(clone, attributesToRemove)
+      if (tagsToPreserve.includes(childNode.tagName)) {
+        const clone = childNode.cloneNode(true);
+        removeAttributes(clone, attributesToRemove);
 
-        let text = textContent(clone, doc) 
+        let text = textContent(clone, doc);
         if (text.length === 0 && clone.tagName === 'A') {
-          const fileName = clone.href.substring(clone.href.lastIndexOf('/') + 1)
-          text = `[${fileName}]`
+          const fileName = clone.href.substring(clone.href.lastIndexOf('/') + 1);
+          text = `[${fileName}]`;
         }
 
-        clone.innerHTML = text
-        results.push(clone.outerHTML)
-        return
+        clone.innerHTML = text;
+        results.push(clone.outerHTML);
+        return;
       }
-      // Convert `textContentTagsToConvertToBold` to bold  
-      if(textContentTagsToConvertToBold.includes(childNode.tagName)) {
-        const b = doc.createElement('B')
-        b.innerHTML = textContent(childNode, doc)
-        results.push(b.outerHTML)
-        return
+      // Convert `textContentTagsToConvertToBold` to bold
+      if (textContentTagsToConvertToBold.includes(childNode.tagName)) {
+        const b = doc.createElement('B');
+        b.innerHTML = textContent(childNode, doc);
+        results.push(b.outerHTML);
+        return;
       }
       results.push(textContent(childNode, doc));
     }
-  })
-  return results.join(''); 
+  });
+  return results.join('');
 }
 
 const arraysOfNodesAroundSignaturesReducer = (resultArray, item, index) => {
   if (index === 0) {
-    resultArray.push([])
+    resultArray.push([]);
   }
   if (item.textContent.length > 0) {
-    resultArray[resultArray.length - 1].push(item)  
+    resultArray[resultArray.length - 1].push(item);
   }
-  const isSigned = signatureRegex.test(item.textContent)
+  const isSigned = signatureRegex.test(item.textContent);
   if (isSigned) {
-    resultArray.push([])
+    resultArray.push([]);
   }
-  return resultArray
-}
+  return resultArray;
+};
 
 const pContainingArrayOfNodes = (nodeArray, doc) => {
-  const p = doc.createElement('p')
-  nodeArray.forEach(p.appendChild, p)
-  return p 
-}
+  const p = doc.createElement('p');
+  nodeArray.forEach(p.appendChild, p);
+  return p;
+};
 
 const soughtElementsInSection = (sectionElement, doc) => {
-  let elements = []
+  let elements = [];
   Array.from(sectionElement.querySelectorAll('p,li,dt,dd'))
     .forEach(element => {
-      if (element.tagName === 'P'){
+      if (element.tagName === 'P') {
         // Sometimes inside 'p' tags we see responses only separated by break tags, so wrap these
         // responses in 'p' elements so they are treated like 'p' wrapped responses.
         element.childNodes
           .reduce(arraysOfNodesAroundSignaturesReducer, [])
           .map(nodes => pContainingArrayOfNodes(nodes, doc))
-          .forEach(p => elements.push(p))
-      }else{
-        elements.push(element)  
+          .forEach(p => elements.push(p));
+      } else {
+        elements.push(element);
       }
-    })  
-  return elements
-}
+    });
+  return elements;
+};
 
 class WMFSection {
   constructor(sectionElement, doc) {
-    this.id = sectionElement.getAttribute('data-mw-section-id')
-    this.items = this.itemsFromSectionElement(sectionElement, doc)
+    this.id = sectionElement.getAttribute('data-mw-section-id');
+    this.items = this.itemsFromSectionElement(sectionElement, doc);
     // Question: h2 only?
-    const h2 = sectionElement.querySelector('h2')
+    const h2 = sectionElement.querySelector('h2');
     // this.text = h2 ? h2.textContent : ''
-    const titleHTMLExclusions = ['A']
-    this.text = textContent(h2, doc, titleHTMLExclusions)
+    const titleHTMLExclusions = ['A'];
+    this.text = textContent(h2, doc, titleHTMLExclusions);
     // Section sha on section title and items sha's.
-    this.sha = createSha1(`${this.text}${this.items.map(item => item.sha).join('')}`)
+    this.sha = createSha1(`${this.text}${this.items.map(item => item.sha).join('')}`);
   }
   itemsFromSectionElement (sectionElement, doc) {
     return soughtElementsInSection(sectionElement, doc)
       .reverse()
       .map(item => {
-          const fragment = doc.createDocumentFragment()
-          const depth = getDepth(item)
-          fragment.appendChild(item)
-          return new WMFMessageFragmentAndDepth(fragment, depth)
+          const fragment = doc.createDocumentFragment();
+          const depth = getDepth(item);
+          fragment.appendChild(item);
+          return new WMFMessageFragmentAndDepth(fragment, depth);
       })
       .filter((fragmentAndDepth, index, array) => combiner(fragmentAndDepth, index, array, doc))
       .reverse()
       .map(fragmentAndDepth => new WMFMessage(textContent(fragmentAndDepth.fragment, doc), fragmentAndDepth.depth))
-      .filter(m => m.text.length > 0)
+      .filter(m => m.text.length > 0);
   }
   shortenShas() {
-    shortenSha(this)
-    this.items.forEach(shortenSha)  
+    shortenSha(this);
+    this.items.forEach(shortenSha);
   }
 }
 
 // Reduce section or item sha to first 7 chars.
-const shortenSha = sectionOrItem => sectionOrItem.sha = sectionOrItem.sha.substring(0, 7)
+const shortenSha = sectionOrItem => sectionOrItem.sha = sectionOrItem.sha.substring(0, 7);
 
 const sectionsInDoc = doc => Array.from(doc.querySelectorAll('section'))
-  //.filter((e, i) => i === 37) // Debugging a single section by index 
-  .map(sectionElement => new WMFSection(sectionElement, doc))
+  // .filter((e, i) => i === 37) // Debugging a single section by index
+  .map(sectionElement => new WMFSection(sectionElement, doc));
 
 function fetchAndRespond(app, req, res) {
   const lang = req.params.domain.split('.')[0];
-  // assertLanguage(lang); 
+  // assertLanguage(lang);
   return fetchDocAndRevision(app, req)
     .then((docAndRevision) => {
         const doc = docAndRevision[0];
         const revision = docAndRevision[1];
-        const sections = sectionsInDoc(doc)
-        sections.forEach(section => section.shortenShas())
+        const sections = sectionsInDoc(doc);
+        sections.forEach(section => section.shortenShas());
         endResponseWithOutput(app, res, sections, req.params.domain, revision);
     });
 }
