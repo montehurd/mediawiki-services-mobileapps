e7ebb5171473d9afd86d76cf135ba7b4f045c47d
montehurd <mhurd@wikimedia.org>
2019-05-10 19:15:49 -0700

    Endpoint to deliver structured user talk page data to apps
    Teases out talk page topics and replies (their respective depths) destined for native app consumption.
    Delivers just a small subset of html tags - rest are delivered as text. Details on phab ticket.
    
    - The change to 'package.json' is temp and will be removed.
    - I still need to add more tests!
    - The '.on-save' files are temp and will be removed.
    - Name 'talk' is very open to suggestions :)
    
    Bug: T221148
    Change-Id: I0a762ee166f8399d680bd7755f4370b6cfd92d3b

---
+++ routes/page/talk.js
@@ -0,0 +1,24 @@
+const router = require('../../lib/util').router();
+const talk = require('../../lib/talk');
+
+/**
+ * The main application object reported when this module is required.
+ */
+let app;
+
+/**
+ * GET {domain}/v1/page/talk/{title}{/revision}{/tid}
+ * Gets talk page info.
+ */
+router.get('/talk/:title/:revision?/:tid?', (req, res) => {
+  return talk.fetchAndRespond(app, req, res);
+});
+
+module.exports = function(appObj) {
+    app = appObj;
+    return {
+        path: '/page',
+        api_version: 1,
+        router
+    };
+};
